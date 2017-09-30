274ccd9f32d69515a1ec8cdcad17f812e1046a2c
cipepser <respepic@gmail.com>
2017-09-30 17:32:58 +0900

    add alias.zsh

---
+++ zsh/alias.zsh
@@ -0,0 +1,19 @@
+# shell
+alias ll='ls -l'
+alias la='ls -la'
+
+# git
+alias g='git'
+alias ga='git add'
+alias gc='git commit'
+alias gp='git push'
+alias gs='git status'
+
+# docker
+alias d='docker'
+alias dm='docker-machine'
+alias dp='docker ps'
+alias di='docker images'
+
+# python
+alias p='python'
