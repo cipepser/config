e1dc31e7c4ba05a89e7f19925dc99051d88d7157
cipepser <respepic@gmail.com>
2017-09-30 15:51:53 +0900

    clean up

--- mac_init.sh
+++ mac_init.sh
@@ -1,30 +1,55 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # brew
-brew cask install iter2
-
-brew cask install skype
-brew cask install --caskroom=/Applications google-chrome  # sanbox機能を使うので/Applicationsに入れる必要がある
-brew cask install google-drive
-brew cask install google-japanese-ime
-
-brew cask install bettertouchtool # ウィンドウリサイズとかのショートカットキー設定
-brew cask install appcleaner #アプリのアンインストーラー
-brew cask install flux #輝度調節
-brew cask install bartender #タスクバーに一杯出てくるアイコンを収納してくれるアプリ
-brew cask install slack
-brew cask install alfred # spotlightの強化版みたいなやつ
-brew cask install atom
-brew cask install karabiner # キーリマップ
-
-brew cask install symboliclinker
+brew install iter2
+brew install skype
+brew install --caskroom=/Applications google-chrome  # sanbox機能を使うので/Applicationsに入れる必要がある
+brew install google-drive
+brew install google-japanese-ime
+brew install bettertouchtool # ウィンドウリサイズとかのショートカットキー設定
+brew install appcleaner #アプリのアンインストーラー
+brew install flux #輝度調節
+brew install bartender #タスクバーに一杯出てくるアイコンを収納してくれるアプリ
+brew install slack
+brew install alfred # spotlightの強化版みたいなやつ
+brew install atom
+brew install karabiner # キーリマップ
+brew install symboliclinker
 brew install nodejs
 brew install git
+brew install ghq
+brew intall peco # fish上で、ctrl+rでコマンド履歴を呼び出すため
+brew install hub
 
-# set path to this config derectory
-# シンボリックリンクを貼るためなので、一時的でOK
-# 実行はgit cloneしたconfig repo直下で行うこと
-set -x CONFIG (pwd)
+# fish
+brew install fish
+which fish | xargs chsh -s
+## nos-standard shellのエラーのときのみ
+## sudo echo 'usr/local/bin/fish' > /etc/shells
+## oh-my-fish
+curl -L http://get.oh-my.fish | fish
+rm ~/.config/fish/config.fish
+ln -s  $CONFIG/fish/config.fish ~/.config/fish/config.fish
+
+## fisherman
+curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
+
+## z (for fish)
+## 最近移動したディレクトリをpecoで拾うため
+fisher z
+
+
+# zsh
+brew install zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting colordiff reattach-to-user-namespace tmux
+which zsh | xargs chsh -s
+
+## zplug プラグイン管理のため
+git clone https://github.com/zplug/zplug.git .zplug
+
+## z for zsh
+## 移動したディレクトリを覚えておくため
+git clone https://github.com/rupa/z.git ~/.zsh.d
+
 
 # git
 git config --global user.name "cipepser"
@@ -34,34 +59,9 @@ git config --global alias.co checkout
 rm ~/.gitconfig
 ln -s $CONFIG/git/.gitconfig ~/.gitconfig
 
-# fish
-brew install fish
-chsh -s /usr/local/bin/fishs
-# nos-standard shellのエラーのときのみ
-# sudo echo 'usr/local/bin/fish' > /etc/shells
-# oh-my-fish
-curl -L http://get.oh-my.fish | fish
-rm ~/.config/fish/config.fish
-ln -s  $CONFIG/fish/config.fish ~/.config/fish/config.fish
-
-## fisherman
-curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
-
-# z (for fish)
-# 最近移動したディレクトリをpecoで拾うため
-fisher z
-
 # atom
 rm ~/.atom/keymap.cson
 ln -s $CONFIG/atom/keymap.cson ~/.atom/keymap.cson
-
-# gpq
-brew install ghq
-# peco
-brew intall peco # fish上で、ctrl+rでコマンド履歴を呼び出すため
-# hub
-brew install hub
-
 
 # npm
 npm install -g jshint
