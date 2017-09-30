bf5fde802a23f9fecd8abe60fc5261006cf54f68
cipepser <respepic@gmail.com>
2017-09-30 21:55:49 +0900

    fixed zplug

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -6,20 +6,11 @@
 source ~/.zplug/init.zsh
 zplug 'zplug/zplug', hook-build:'zplug --self-manage'
 
-# TODO: theme選び
-# theme (https://github.com/sindresorhus/pure#zplug)
 zplug "mafredri/zsh-async"
 zplug "sindresorhus/pure"
 
-# zplug "yous/lime"
-
-# zplug "mollifier/anyframe"
-
 # 構文のハイライト(https://github.com/zsh-users/zsh-syntax-highlighting)
 zplug "zsh-users/zsh-syntax-highlighting"
-
-# # history関係
-# zplug "zsh-users/zsh-history-substring-search"
 
 # タイプ補完
 zplug "zsh-users/zsh-autosuggestions"
@@ -34,7 +25,6 @@ if ! zplug check --verbose; then
   fi
 fi
 
-
 #--------------------------------------
 # Command history configuration
 #--------------------------------------
@@ -46,9 +36,9 @@ setopt hist_ignore_space # ignore when commands starts with space
 setopt share_history     # share command history data
 
 function peco_select_history() {
-    BUFFER=`history -n 1 | tail -r  | awk '!a[$0]++' | peco`
-    CURSOR=$#BUFFER
-    zle reset-prompt
+  BUFFER=`history -n 1 | tail -r  | awk '!a[$0]++' | peco`
+  CURSOR=$#BUFFER
+  zle reset-prompt
 }
 zle -N peco_select_history
 bindkey '^r' peco_select_history
@@ -56,34 +46,35 @@ bindkey '^r' peco_select_history
 #--------------------------------------
 # change directory you visit recently
 #--------------------------------------
-
-# TODO: 実装する
 source ~/.zsh.d/z.sh
 
 function peco_recentd() {
-  
-    # やりたい処理
-    # if [[ $PWD = $HOME ]];then
-    #   cd $(find . -maxdepth 3 -type d ! -path "*/.*" | cat | peco | sed "s|~|$HOME|")
-    # else
-    #   cd $(find . -maxdepth 4 -type d ! -path "*/.*" | cat | peco | sed "s|~|$HOME|")
-    # fi
-    # キー実行時のプロンプトの内容は $BUFFER で取れる
-    zle reset-prompt  # プロンプトを再描画
- }
-zle -N peco_recentd  # my_functionをwidgetとして登録
-bindkey '^x' peco_recentd # my_functionをCtrl-Aにバインド
+  rd=`z -l | peco | awk '{ print $2 }'`
+  if [ -n "$rd" ]; then
+    BUFFER+="cd $rd"
+    zle accept-line
+  else
+    zle reset-prompt
+  fi
+}
+zle -N peco_recentd
+bindkey '^x' peco_recentd
 
 #--------------------------------------
 # ghq directory
 #--------------------------------------
+function select_ghq_cd() {
+  repo=`ghq list --full-path | peco | awk '{ print $1 }'`
 
-
-# TODO: 実装する
-
-
-
-
+  if [ -n "$repo" ]; then
+    BUFFER+="cd $repo"
+    zle accept-line
+  else
+    zle reset-prompt
+  fi
+}
+zle -N select_ghq_cd
+bindkey '^g' select_ghq_cd
 
 
 # Then, source plugins and add commands to $PATH
