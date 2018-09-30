572f1e649f4cb240f0d9cbfa8b298ce257642108
cipepser <respepic@gmail.com>
2018-10-01 07:47:30 +0900

    update

--- zsh/prezto.zsh
+++ zsh/prezto.zsh
@@ -2,3 +2,15 @@
 if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
   source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
 fi
+
+# modules
+# zstyle ':prezto:load' pmodule \
+#   'archive' \
+#   'environment' \
+#   'terminal' \
+#   'editor' \
+#   'history' \
+#   'directory' \
+#   'spectrum' \
+#   'completion' \
+#   'prompt'
