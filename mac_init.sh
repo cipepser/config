ab3136337514f78d0a796f7878f5296e6b434aea
cipepser <respepic@gmail.com>
2019-07-23 07:46:18 +0900

    update mac_init.sh

--- mac_init.sh
+++ mac_init.sh
@@ -1,88 +1,81 @@
+# brew本体
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+git -C "$(brew --repo homebrew/core)" fetch --unshallow
 
-# brew
-brew install iter2
-brew install skype
-brew install --caskroom=/Applications google-chrome  # sanbox機能を使うので/Applicationsに入れる必要がある
-brew install google-drive
-brew install google-japanese-ime
-brew install bettertouchtool # ウィンドウリサイズとかのショートカットキー設定
-brew install appcleaner #アプリのアンインストーラー
-brew install flux #輝度調節
-brew install bartender #タスクバーに一杯出てくるアイコンを収納してくれるアプリ
-brew install slack
-brew install alfred # spotlightの強化版みたいなやつ
-brew install atom
-brew install karabiner # キーリマップ
-brew install symboliclinker
-brew install nodejs
-brew install git
-brew install ghq
-brew intall peco # fish上で、ctrl+rでコマンド履歴を呼び出すため
-brew install hub
+# ディレクトリ
+mkdir $HOME/.go
+mkdir $HOME/Documents/sand
 
-# fish
-brew install fish
-which fish | xargs chsh -s
-## nos-standard shellのエラーのときのみ
-## sudo echo 'usr/local/bin/fish' > /etc/shells
-## oh-my-fish
-curl -L http://get.oh-my.fish | fish
-rm ~/.config/fish/config.fish
-ln -s  $CONFIG/fish/config.fish ~/.config/fish/config.fish
+# git（事前）
+git config --global user.name "cipepser"
+git config --global user.email "respepic@gmail.com"
+git config --global color.ui auto
+git config --global alias.co checkout
 
-## fisherman
-curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
-
-## z (for fish)
-## 最近移動したディレクトリをpecoで拾うため
-fisher z
-
+cd $HOME/Documents
+git clone https://github.com/cipepser/config.git
 
 # zsh
 brew install zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting colordiff reattach-to-user-namespace tmux
 which zsh | xargs chsh -s
 
 ## zplug プラグイン管理のため
+cd $HOME
 git clone https://github.com/zplug/zplug.git .zplug
 
 ## z for zsh
 ## 移動したディレクトリを覚えておくため
 git clone https://github.com/rupa/z.git ~/.zsh.d
 
+# /etc/shellsの末尾に`/usr/local/bin/zsh`を追記
+chsh -s /usr/local/bin/zsh
+exec $SHELL -l
 
 # git
-git config --global user.name "cipepser"
-git config --global user.email "respepic@gmail.com"
-git config --global color.ui auto
-git config --global alias.co checkout
 rm ~/.gitconfig
 ln -s $CONFIG/git/.gitconfig ~/.gitconfig
 
-# atom
-rm ~/.atom/keymap.cson
-ln -s $CONFIG/atom/keymap.cson ~/.atom/keymap.cson
-
-# npm
-npm install -g jshint
-
-# wireshark
-# インストーラで入れた
+# brew
+brew install ghq
+brew install peco
+brew install nodejs
+brew install hub
+brew install jq
+brew install go
+brew install ripgrep
+brew install direnv
+brew install zplug
+brew install tree
+brew install gibo
+brew install graphviz
 
 # ctags
 brew install ctags
 alias ctags="`brew --prefix`/bin/ctags"
+
+# zsh
+rm $HOME/.zshrc
+ln -s $HOME/Documents/config/zsh/.zshrc $HOME/.zshrc
 
 # fzf
 # ghqしたrepoへcdするために必要
 git clone https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-# mycli
-brew install mycli
-rm ~/.myclirc
-ln -s $CONFIG/mycli/.myclirc ~/.myclirc
+# atom
+rm ~/.atom/keymap.cson
+ln -s $CONFIG/atom/keymap.cson ~/.atom/keymap.cson
 
-# ghkw
-brew tap kyoshidajp/ghkw
-brew install ghkw
+# rust
+curl https://sh.rustup.rs -sSf | sh
+source $HOME/.cargo/env
+
+# karabiner
+ln -s $CONFIG/karabiner/ctrl-kana-to-esc.json $HOME/.config/karabiner/assets/complex_modifications/ctrl-kana-to-esc.json
+
+# カーソル速度
+defaults write -g KeyRepeat -int 1
+defaults write -g InitialKeyRepeat -int 12
+
+# 隠しファイルの表示
+defaults write com.apple.finder AppleShowAllFiles TRUE 
